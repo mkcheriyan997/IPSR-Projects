bd431078a82ceb6080d721dbd9a679058fe787fc
EC2 Default User <ec2-user@ip-172-31-19-105.eu-north-1.compute.internal>
2026-06-16 05:46:07 +0000

    Add new Blue-Green Deployment project

---
+++ jenkins-blue-green-project/app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+VERSION = os.environ.get('APP_VERSION', '1.0')
+COLOR = os.environ.get('APP_COLOR', 'blue')
+
+@app.route('/')
+def hello():
+    return f"""
+    <body style="background-color: {COLOR}; color: white; text-align: center; padding-top: 100px; font-family: sans-serif;">
+        <h1>Application Version {VERSION}</h1>
+        <p>This is the <strong>{COLOR.upper()}</strong> environment.</p>
+    </body>
+    """
+
+@app.route('/health')
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
